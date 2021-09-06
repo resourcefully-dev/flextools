75a84f69624938d607004b1a4a2c3f6a7a01cc54
Marc Cañigueral <m.canigueral@resourcefully.nl>
2021-09-06 09:39:42 +0200

    Introduction of Profile's Window

--- R/smart_charging.R
+++ R/smart_charging.R
@@ -60,7 +60,7 @@ smart_charging <- function(sessions, fitting_data, method, window_length, window
   #   2. responsive values higher than 0
   #   3. optimization weight higher than 0
   opt_profiles <- names(responsive)[
-    (names(responsive) %in% unique(sessions_norm[['Profile']])) &
+    (names(responsive) %in% unique(sessions_norm$Profile)) &
       (names(responsive) %in% names(responsive)[as.numeric(responsive) > 0]) &
       (names(responsive) %in% names(opt_weights)[as.numeric(opt_weights) > 0])
   ]
@@ -70,24 +70,23 @@ smart_charging <- function(sessions, fitting_data, method, window_length, window
   setpoints <- profiles_demand
 
   print(sessions_norm)
+  print(unique(sessions_norm$Profile))
+  print(paste('Profiles:', opt_profiles))
   # For each optimization window
   for (i in seq(1, length(dttm_seq), window_length)) {
     window <- c(i, i+window_length-1)
-    print(paste('Profiles:', opt_profiles, '--- Window:', window))
 
     # For each optimization profile
     for (profile in opt_profiles) {
 
       # Filter only Profile's sessions that start and finish CHARGING within the time window
       sessions_prof_window <- sessions_norm %>% filter(.data$Profile == profile, .data$chs >= window[1], .data$che < window[2])
-      print(sessions_prof_window)
 
       # Limit the CONNECTION end time to the windows's end timeslot
       sessions_prof_window$coe[(sessions_prof_window$coe > window[2])] <- window[2]
       sessions_prof_window$f <- (sessions_prof_window$coe - sessions_prof_window$chs) - (sessions_prof_window$che - sessions_prof_window$chs)
 
       # Re-define window to profile's connection window
-      print(paste('Minimum:', min(sessions_prof_window$cos), '---- Maximum:', max(sessions_prof_window$coe)))
       window_prof <- c(min(sessions_prof_window$cos), max(sessions_prof_window$coe))
       window_prof_idxs <- (fitting_data_norm$timeslot >= window_prof[1]) & (fitting_data_norm$timeslot <= window_prof[2])
       window_prof_length <- window_prof[2] - window_prof[1] + 1

5103f23f724742f49e19ab039159c89873105f79
Marc Cañigueral <marccanyigueral@gmail.com>
2022-03-02 17:26:12 +0100

    Bug fix: select variables that are not in normalized sessions

--- R/smart_charging.R
+++ R/smart_charging.R
@@ -194,12 +194,12 @@ smart_charging <- function(sessions, fitting_data, method, window_length, window
     }
   }
 
+  denormalized_sessions <- denormalize_sessions(sessions_norm, start, time_interval)
   sessions_opt <- left_join(
-    sessions['Session'],
-    denormalize_sessions(sessions_norm, start, time_interval),
+    denormalized_sessions,
+    select(sessions, 'Session', !any_of(names(denormalized_sessions))),
     by = 'Session'
-  ) %>%
-    select(names(sessions), everything())
+  )
 
   return(list(
     setpoints = denormalize_timeseries(setpoints, start, time_interval),

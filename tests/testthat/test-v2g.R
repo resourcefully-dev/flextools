82dda7ba8ef84399f43ad6ae9752347eda25a3f4
Marc <marccanyigueral@gmail.com>
2025-12-19 17:38:57 +0100

    removing devtools dependency

--- tests/testthat/test-v2g.R
+++ tests/testthat/test-v2g.R
@@ -1,5 +1,4 @@
 library(dplyr)
-devtools::load_all()
 
 # Use first 50 sessions
 sessions <- evsim::california_ev_sessions_profiles %>%
@@ -7,7 +6,7 @@ sessions <- evsim::california_ev_sessions_profiles %>%
     mutate(Power = Power * 2) |>
     evsim::adapt_charging_features(time_resolution = 15) |>
     filter(
-        Profile == "Worktime", 
+        Profile == "Worktime",
         date(ConnectionStartDateTime) == as.Date("2018-10-08")
     )
 sessions_demand <- evsim::get_demand(sessions, resolution = 15) |>
@@ -18,19 +17,29 @@ sessions_demand <- evsim::get_demand(sessions, resolution = 15) |>
 opt_data <- tibble(
     datetime = sessions_demand$datetime,
     production = 0,
-    import_capacity = c(rep(c(
-        rep(100, 60 / 15 * 7), # 00:00-07:00
-        rep(-20, 60 / 15 * (11 - 7)), # 07:00-11:00
-        rep(100, 60 / 15 * (24 - 11)) # 11:00-24:00
-    ), times = 1), 100) # 3 days + 1 extra time step
+    import_capacity = c(
+        rep(
+            c(
+                rep(100, 60 / 15 * 7), # 00:00-07:00
+                rep(-20, 60 / 15 * (11 - 7)), # 07:00-11:00
+                rep(100, 60 / 15 * (24 - 11)) # 11:00-24:00
+            ),
+            times = 1
+        ),
+        100
+    ) # 3 days + 1 extra time step
 )
 # timefully::plot_ts(opt_data)
 
 test_that("optimization for v2g works and negative import capacity is achieved", {
     v2g_results <- smart_v2g(
-        sessions = sessions, opt_data, opt_objective = "grid",
-        window_days = 1, window_start_hour = 0,
-        show_progress = TRUE, include_log = TRUE
+        sessions = sessions,
+        opt_data,
+        opt_objective = "grid",
+        window_days = 1,
+        window_start_hour = 0,
+        show_progress = TRUE,
+        include_log = TRUE
     )
     # plot_smart_charging(v2g_results, sessions, legend_width = 150)
     # view_smart_charging_logs(v2g_results)
